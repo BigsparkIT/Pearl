b2444aeab09491a1cef1f324164a87c6bb330680
Loth Janssen <loth@bigspark.com>
2025-03-06 10:43:20 +0100

    only inject platform name if user is admin

--- wp-content/plugins/pearl/pearl.php
+++ wp-content/plugins/pearl/pearl.php
@@ -17,16 +17,18 @@ if (! defined('ABSPATH')) {
  * Plugin initialisatie.
  */
 add_action('init', function () {
-    // Add the platformName to the code to give the frontend access to the value.
-    wp_enqueue_script('pearl-product-iframe-block', plugins_url('build/blocks/product-iframe/index.js', __FILE__));
-    wp_add_inline_script('pearl-product-iframe-block', 'const pearlSettings = ' . json_encode([
-        'platformName' => get_option('pearl_platform_identifier', ''),
-    ]), 'before' );
-
     register_block_type(__DIR__ . '/build/blocks/product-iframe');
 });
 
 // Laad de settings-page code enkel in de admin omgeving.
 if (is_admin()) {
     require_once plugin_dir_path(__FILE__) . 'includes/settings-page.php';
+
+    add_action('init', function () {
+        // Add the platformName to the code to give the frontend access to the value.
+        wp_enqueue_script('pearl-product-iframe-block', plugins_url('build/blocks/product-iframe/index.js', __FILE__));
+        wp_add_inline_script('pearl-product-iframe-block', 'const pearlSettings = ' . json_encode([
+            'platformName' => get_option('pearl_platform_identifier', ''),
+        ]), 'before' );
+    });
 }
